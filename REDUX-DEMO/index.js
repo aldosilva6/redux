c2034a1d9c262b665a883565816b0f6548340292
Aldo Inácio da Silva <aldosilva6@gmail.com>
2020-08-05 20:59:32 -0300

    Added studies of multiple reducers

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -5,8 +5,10 @@ const redux = require('redux')
 
 //1 - Create the store
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 //1) - Action creator is a function that return an action
 function buyCake(){
@@ -17,13 +19,28 @@ function buyCake(){
     }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
 //Firt parameter of reducer
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+const initialCakeState = {
     numOfCakes: 10
 }
 
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
 //2) - A function that accepts state and action as arguments, and returns the next state of the application
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             //Use spread operator to make a copy of the state and only change the numOfCakes property
@@ -35,9 +52,27 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+
+        case BUY_ICECREAM: return {
+            //Use spread operator to make a copy of the state and only change the numOfCakes property
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
+
+        default: return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+
 //The create store receive a parameter which is the reducer that holds the application state
 //1 - A store to hold the state application
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 //2
 console.log('Initial state', store.getState())
 //4 and 5 - A listener to the store - Anytime the state updates we log the state to the console
@@ -46,7 +81,9 @@ const unsubscribe = store.subscribe(() => console.log('Update state', store.getS
 store.dispatch(buyCake()) // When we dispatch the action the reducer sees that the action type is BUY_CAKE
 store.dispatch(buyCake())
 store.dispatch(buyCake())
-unsubscribe()
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+unsubscribe() // Unsubscribe to any change in the store
 
 //Redux Store Responsabilities
 //1 - Holds application state
